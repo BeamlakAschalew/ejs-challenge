a5b966764f1b87396405609f26554c5c92049223
Beamlak Aschalew <birrletej12@gmail.com>
2023-12-21 19:21:21 +0300

    finished ejs

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -18,8 +19,47 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const posts = [];
+
 app.get("/", (req, res) => {
-  res.render("home", { content: homeStartingContent });
+  res.render("home", { content: homeStartingContent, posts: posts });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { contactContent: contactContent });
+});
+
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+
+app.post("/compose", (req, res) => {
+  const post = {
+    title: req.body.title,
+    content: req.body.content,
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
+});
+
+app.get("/posts/:postId", (req, res) => {
+  posts.forEach((post) => {
+    if (
+      (post.title = _.toLower(post.title)) ===
+      (req.params.postId = _.toLower(req.params.postId))
+    ) {
+      console.log("found");
+      res.render("post", { post: post });
+    } else {
+      console.log("Not found");
+    }
+  });
 });
 
 app.listen(3000, function () {
